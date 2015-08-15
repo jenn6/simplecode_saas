5ae7a05bd691707ea9e4c17a1cef6ce9f2a0fb13
Jenn Six <jenn6design@gmail.com>
2015-08-15 20:02:27 +0000

    Added message alerts for contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,20 @@ class ContactsController < ApplicationController
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save
+            flash[:success] = "Great! You sent a message :)"
+            redirect_to new_contact_path
+        else
+            flash[:error] = "Uh oh, your message did NOT send :("            
+            redirect_to new_contact_path
+        end
     end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
     
 end
